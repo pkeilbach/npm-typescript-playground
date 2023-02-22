2594592c9316074922b0d38271b6654ecab31a72
Pascal Keilbach <pascal.keilbach@sap.com>
2023-02-22 16:08:05 +0100

    add more typescript test types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+export enum ContractStatus {
+    Permanent,
+    Temp,
+    Apprentice
+};
+
+export type testResult = "pass" | "fail" | "incomplete";
+
 export const Greeter = (name: string) => `Hello ${name}`;
